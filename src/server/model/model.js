2eb4fa897d5ed2d7ae8527fb3c200f3a1c56b1ae
pddivine <pddivine@gmail.com>
2016-10-28 14:28:20 -0700

    Routes and route data craeted

--- src/server/model/model.js
+++ src/server/model/model.js
@@ -1,10 +1,10 @@
-var Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize('endeavor', 'pdivine', 'ilovetesting', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
-var User = sequelize.define('user', {
+const User = sequelize.define('user', {
   id: {
     autoIncrement: true,
     type: Sequelize.INTEGER,
@@ -16,8 +16,122 @@ var User = sequelize.define('user', {
   email_address: {
     type: Sequelize.TEXT
   },
-  pw: {
+  password: {
     type: Sequelize.TEXT
+  },
+  createdAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+ updatedAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+});
+
+const Project = sequelize.define('project', {
+  id: {
+    autoIncrement: true,
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.TEXT
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  github_link: {
+    type: Sequelize.TEXT
+  },
+  createdAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+ updatedAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+});
+
+const Interest = sequelize.define('interest', {
+  id: {
+    autoIncrement: true,
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  keyword: {
+    type: Sequelize.TEXT
+  },
+  createdAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+ updatedAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+});
+
+// Join tables
+const User_Interest = sequelize.define('user_interest', {
+  id: {
+    autoIncrement: true,
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  user_id: {
+    type: Sequelize.INTEGER
+  },
+  interest_id: {
+    type: Sequelize.INTEGER
+  }, 
+  createdAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+ updatedAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+});
+
+const User_Project = sequelize.define('user_project', {
+  id: {
+    autoIncrement: true,
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  user_id: {
+    type: Sequelize.INTEGER
+  },
+  project_id: {
+    type: Sequelize.INTEGER
+  },
+  like: {
+    type: Sequelize.BOOLEAN
+  }, 
+  createdAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+ updatedAt: {
+   type: Sequelize.DATE,
+   default: Date.now
+ },
+});
+
+const Project_Interest = sequelize.define('project_interest', {
+  id: {
+    autoIncrement: true,
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  project_id: {
+    type: Sequelize.INTEGER
+  },
+  interest_id: {
+    type: Sequelize.INTEGER
   },
   createdAt: {
    type: Sequelize.DATE,
@@ -37,28 +151,143 @@ sequelize.sync({
     {
       first_name: 'hoon',
       email_address: 'patrick@email.com',
-      pw: 'a'
+      password: 'a'
     },
     {
       first_name: 'nick',
       email_address: 'patrick@email.com',
-      pw: 'a'
+      password: 'a'
     },
     {
       first_name: 'dan',
       email_address: 'patrick@email.com',
-      pw: 'a'
+      password: 'a'
     },
     {
       first_name: 'patrick',
       email_address: 'patrick@email.com',
-      pw: 'a'
+      password: 'a'
     },
   ];
+
+  const projects = [
+    {
+      name: 'Farmville',
+      description: 'Build a farm',
+      github_link: 'http://www.google.com'
+    },
+    {
+      name: 'Hackernews',
+      description: 'Software engineering blog.',
+      github_link: 'http://www.google.com'
+    },
+    {
+      name: 'Website',
+      description: 'Build a website',
+      github_link: 'http://www.google.com'
+    },
+  ];
+
+  const interests = [
+    {
+      keyword: 'Javascript',
+    },
+    {
+      keyword: 'React',
+    },
+    {
+      keyword: 'ES6',
+    },
+  ];
+
+  const user_interests = [
+    {  user_id: '1',
+      interest_id: '1'
+    },
+    {  user_id: '1',
+      interest_id: '2'
+    },
+    {  user_id: '2',
+      interest_id: '3'
+    },
+    {  user_id: '3',
+      interest_id: '1'
+    },
+    {  user_id: '3',
+      interest_id: '3'
+    },
+    {  user_id: '4',
+      interest_id: '2'
+    },
+  ];
+  const user_projects = [
+    {  user_id: '1',
+      project_id: '1'
+    },
+    {  user_id: '1',
+      project_id: '2'
+    },
+    {  user_id: '2',
+      project_id: '3'
+    },
+    {  user_id: '3',
+      project_id: '1'
+    },
+    {  user_id: '3',
+      project_id: '3'
+    },
+    {  user_id: '4',
+      project_id: '2'
+    },
+  ];
+  const project_interests = [
+    {  interest_id: '1',
+      project_id: '1'
+    },
+    {  interest_id: '1',
+      project_id: '2'
+    },
+    {  interest_id: '2',
+      project_id: '3'
+    },
+    {  interest_id: '3',
+      project_id: '1'
+    },
+    {  interest_id: '3',
+      project_id: '3'
+    },
+  ];
+
   User.bulkCreate(users, {validate: true})
   .catch((err) => {
     console.log('Create Error: ', err);
   });
+
+  Project.bulkCreate(projects, {validate: true})
+  .catch((err) => {
+    console.log('Create Error: ', err);
+  });
+
+  Interest.bulkCreate(interests, {validate: true})
+  .catch((err) => {
+    console.log('Create Error: ', err);
+  });
+
+  User_Interest.bulkCreate(user_interests, {validate: true})
+  .catch((err) => {
+    console.log('Create Error: ', err);
+  });
+
+  User_Project.bulkCreate(user_projects, {validate: true})
+  .catch((err) => {
+    console.log('Create Error: ', err);
+  });
+
+  Project_Interest.bulkCreate(project_interests, {validate: true})
+  .catch((err) => {
+    console.log('Create Error: ', err);
+  });
+
 });
 
 function getUserData (callback) {}
