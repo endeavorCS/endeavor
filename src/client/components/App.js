28b330125b4b2f496e6f427a9f63a27d5c443f06
Hoon Choi <vhchoi@gmail.com>
2016-10-28 20:47:45 -0700

    updating to keep component work

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Signin from  './Signin';
+import Dashboard from  './Dashboard';
+
+
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      test:'test'
+    });
+  }
+
   render() {
     return (
       <div>
-        Hi!
+        <Signin />
       </div>
     )
   }
