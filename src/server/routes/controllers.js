49fbff4400e3808faf43277f999bcb70918dac03
pddivine <pddivine@gmail.com>
2016-10-28 20:39:17 -0700

    Added in get routes for users and projects.

--- src/server/routes/controllers.js
+++ src/server/routes/controllers.js
@@ -1,9 +1,42 @@
 const model = require('../model/model.js');
 
-function getUserInfo (req, res) {
+function getUserInfoController (req, res) {
   const username = req.params.user;
-  console.log(username);
-  model.getUserData(username, (userData) => res.send(userData));
+  model.getUserData(username, (userData) => res.json(userData));
 }
 
-module.exports = { getUserInfo };
+function getRelatedProjectsController (req, res) {
+  const username = req.params.user;
+  model.getRelatedProjects(username, (projectData) => res.json(projectData));
+}
+
+function getRelatedInterestsController (req, res) {
+  const username = req.params.user;
+  model.getRelatedInterests(username, (projectData) => res.json(projectData));
+}
+
+function getProjectInfoController (req, res) {
+  const project = req.params.project;
+  model.getProjectInfo(project, (projectData) => res.json(projectData));
+}
+
+function getProjectUsersController (req, res) {
+  const project = req.params.project;
+  console.log('project', project);
+  model.getProjectUsers(project, (projectData) => res.json(projectData));
+}
+
+function getProjectInterestsController (req, res) {
+  const project = req.params.project;
+  model.getProjectInterests(project, (projectData) => res.json(projectData));
+}
+
+
+module.exports = { 
+  getUserInfoController,
+  getRelatedProjectsController,
+  getRelatedInterestsController,
+  getProjectInfoController,
+  getProjectUsersController,
+  getProjectInterestsController
+};
--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,15 @@ const routeControllers = require('./routes/controllers.js');
 // Serve static
 app.use(express.static(path.join(__dirname, '../../bin')));
 //Handle gets
-app.get('/user/basic/:user', routeControllers.getUserInfo);
+app.get('/user/basic/:user', routeControllers.getUserInfoController);
+app.get('/user/related_projects/:user', routeControllers.getRelatedProjectsController);
+app.get('/user/related_interests/:user', routeControllers.getRelatedInterestsController);
+// app.get('/user/suggested_projects/:user', routeControllers.getRelatedProjectsController);
+
+app.get('/project/basic/:project', routeControllers.getProjectInfoController);
+app.get('/project/related_users/:project', routeControllers.getProjectUsersController);
+app.get('/project/related_interests/:project', routeControllers.getProjectInterestsController);
+
 // Handle posts
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
